616c410a1d1fc1bc21b6beecdd036232ec01f711
eylamk <eylamkadden@gmail.com>
2022-03-07 17:16:46 +0200

    added design

--- main.js
+++ main.js
@@ -7,13 +7,17 @@ const logger = (req,res,next) => {
     const url = req.url;
 
     console.log(`${method} - ${url} - ${time}`);
+    next();
 }
 app.use(logger);
+app.use(express.static('./public'));
 
-app.get('/', (req,res)=>{
-    res.send('hello');
+app.all('*', (req,res)=>{
+    res.status(404).send("page not found 404");
 })
 
 
 
-app.listen(9090);
+app.listen(9090, ()=> {
+    console.log("server is now listening in port 9090");
+});
