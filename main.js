38780350ff35a5818c2ffc6308c9dd10c8a86a91
eylamk <eylamkadden@gmail.com>
2022-03-07 15:51:57 +0200

    added logger

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const express = require(`express`);
 const app = express();
 
+const logger = (req,res,next) => {
+    const method = req.method;
+    const time = new Date();
+    const url = req.url;
+
+    console.log(`${method} - ${url} - ${time}`);
+}
+app.use(logger);
+
 app.get('/', (req,res)=>{
     res.send('hello');
 })
+
+
 
 app.listen(9090);
