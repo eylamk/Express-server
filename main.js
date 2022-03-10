e83da3db83878d36cc6be107e59906d7c991d424
eylamk <eylamkadden@gmail.com>
2022-03-10 15:28:06 +0200

    fixed json not working

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const express = require(`express`);
+const index = require('./routes/index.js');
+const todos = require('./routes/todos.js');
 const app = express();
 
 const logger = (req,res,next) => {
@@ -12,12 +14,14 @@ const logger = (req,res,next) => {
 
 app.use(logger);
 app.use(express.static('./public'));
+app.use(express.json());
+ 
+app.use('/todos',todos);
+app.use('/',index);
 
 app.all('*', (req,res)=>{
     res.status(404).send("page not found 404");
 })
-
-
 
 app.listen(9090, ()=> {
     console.log("server is now listening in port 9090");
